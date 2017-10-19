e5a510b3adfc375ddd025412bacb38a68a68ed4c
Daniel Fulton <dfulton@gladd.jp>
2017-10-19 13:01:59 +0900

    Initial Commit

---
+++ CleanGTDTests/CleanGTDTests.swift
@@ -0,0 +1,36 @@
+//
+//  CleanGTDTests.swift
+//  CleanGTDTests
+//
+//  Created by Daniel Fulton on 2017/10/19.
+//  Copyright © 2017 Daniel Fulton. All rights reserved.
+//
+
+import XCTest
+@testable import CleanGTD
+
+class CleanGTDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
